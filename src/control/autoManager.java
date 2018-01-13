07047d5c0b23e63034948efb6bb359070c2ebbd5
SummitRobotics <summitrobotics@gmail.com>
2018-01-12 16:57:45 -0800

    Got closer to finishing the autoManger. The entire codebased was updated to pheonix last commit but I forgot to add that to the comment.

--- src/control/autoManager.java
+++ src/control/autoManager.java
@@ -2,22 +2,47 @@ package control;
 
 import java.util.List;
 
+import org.usfirst.frc.team5468.polybot.polybot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 public class autoManager {
 
 	private double maxAccel = 400;
 	private double maxVelo = 400;
 	
+	private polybot robot;
+	
+	//TODO Set the error to finish that trajectory point
+	private static double finishedError = 0;
+	
 	private List<Integer> leftTrajectoryList;
 	private List<Integer> rightTrajectoryList;
 	
+	private int trajectoryState;
+	private int lastTrajectoryState;
+	private boolean trajectoryClosed;
+	private int totalTrajectoryPoints;
 	
-	public autoManager( ) {
+	private int fGain;
+	private int pGain;
+	private int iGain;
+	private int dGain;
+	
+	private boolean leftDone;
+	private boolean rightDone;
+	
+	public autoManager(polybot robot) {
+		trajectoryState = 0;
+		trajectoryClosed = false;
+		this.robot = robot;
 		
+		//TODO Implement various gain and sensor settings for the talons
 	}
 	
 	public void addStraight(double distance) {
-		leftTrajectoryList.add(feetToEncoder(distance));
-		rightTrajectoryList.add(feetToEncoder(distance));
+		leftTrajectoryList.add(inchesToEncoder(distance));
+		rightTrajectoryList.add(inchesToEncoder(distance));
 	}
 	
 	public void addArc(double radius, int angle) {
@@ -28,13 +53,52 @@ public class autoManager {
 		//TODO Implement pivot rotation
 	}
 	
-	private int feetToEncoder(double distance) {
-		//TODO Implement this conversion
-		return 0;
+	public void end() {
+		trajectoryClosed = true;
+		if(leftTrajectoryList.size() != rightTrajectoryList.size()) {
+			//TODO implement error logging
+			//The left and right trajectory lists must ALWAYS be the same size. This will stop you from running auto in the future
+			//if there is a mismatch in size.
+		}
+		totalTrajectoryPoints = leftTrajectoryList.size();
+	}
+	
+	private int inchesToEncoder(double distance) {
+		return (int) Math.round(((distance) / (Math.PI*(9)) * 4096));
+	}
+	
+	private void incTrajectoryState() {
+		if(trajectoryState < totalTrajectoryPoints) {
+			lastTrajectoryState = trajectoryState;
+			trajectoryState++;
+		}
+		
 	}
 	
 	public void run() {
-		//TODO Implement this
+		
+		leftDone = false;
+		rightDone = false;
+		
+		//Tell the encoders the values to move to again
+		robot.flDrive.set(ControlMode.MotionMagic, leftTrajectoryList.get(trajectoryState));
+		robot.frDrive.set(ControlMode.MotionMagic, rightTrajectoryList.get(trajectoryState));
+		
+		//Check if the left side has reached its target
+		if(robot.flDrive.getClosedLoopError(0) < finishedError) {
+			leftDone = true;
+		} 
+		
+		//Then check the right side
+		if(robot.frDrive.getClosedLoopError(0) < finishedError) {
+			rightDone = true;
+		} 
+				
+		
+		//If both the left and right sides have reached their target it is now time to move on
+		if(leftDone && rightDone) {
+			incTrajectoryState();
+		}
 	}
 	
 	
